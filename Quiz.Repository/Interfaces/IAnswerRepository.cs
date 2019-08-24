a54db5e8f8f9e6f1547389f378fb6fb2dcefd0fe
Lucas Martins <luucasmartins@hotmail.com>
2019-08-24 14:09:45 -0300

    alterações

---
+++ Quiz.Repository/Interfaces/IAnswerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Quiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz.Repository.Interfaces
+{
+    public interface IAnswerRepository : IRepository<Answer>
+    {
+    }
+}
